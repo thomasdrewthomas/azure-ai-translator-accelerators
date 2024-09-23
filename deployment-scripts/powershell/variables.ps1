7f0a6404689ba2166313b94234e8aa9ecbfb0789
thomasdrewthomas <117202574+thomasdrewthomas@users.noreply.github.com>
2024-09-23 17:59:00 +0100

    update-vars

--- deployment-scripts/powershell/variables.ps1
+++ deployment-scripts/powershell/variables.ps1
@@ -5,38 +5,38 @@ function Get-RandomString {
     -join ((97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
 }
 
-$randomString = Get-RandomString
+#$randomString = Get-RandomString
 
-$subscriptionId = ""
-$resourceGroupName = "ai-translator-accelerator-rg-$randomString"
+$subscriptionId = "520838e5-950d-4a4e-905c-e300c984837b"
+$resourceGroupName = "ai-translator-accelerator-rg-vwuif"
 $location = "uksouth"
-$storageAccountName = "azaitranslator$randomString" # Must be between 3 and 24 characters, only lower-case letters and numbers
-$appServicePlanName = "azure-ai-translator-plan-$randomString"
-$functionAppNameUpload = "ai-translator-upload-func-$randomString"
-$functionAppNameTranslate = "ai-translator-translate-func-$randomString"
-$functionAppNameWatermark = "ai-translator-watermark-func-$randomString"
+$storageAccountName = "azaitranslatorvwuif" # Must be between 3 and 24 characters, only lower-case letters and numbers
+$appServicePlanName = "azure-ai-translator-plan-vwuif"
+$functionAppNameUpload = "ai-translator-upload-func-vwuif"
+$functionAppNameTranslate = "ai-translator-translate-func-vwuif"
+$functionAppNameWatermark = "ai-translator-watermark-func-vwuif"
 $storageContainerName = "documents"
 $translationSku = "S1"
-$CosmosDBName = "translateservice$uniqueString"
+$CosmosDBName = "translateservicevwuif"
 # Translation Service variables
-$translationServiceName = "ai-trans-service-$randomString"
-$customDomain = "ai-translator-$randomString"
+$translationServiceName = "ai-trans-service-vwuif"
+$customDomain = "ai-translator-vwuif"
 
 # OpenAI specific variables
-$openAIServiceName = "az-openai-service-$randomString" #$randomString
+$openAIServiceName = "az-openai-service-vwuif" #$randomString
 $openAISku = "S0"
 $chatCompletionsModelName = "gpt-35-turbo" # Adjust as needed
-$chatCompletionsDeploymentName = "az-chat-$randomString"
+$chatCompletionsDeploymentName = "az-chat-vwuif"
 
 # Static Web App variables
-$repositoryUrl = "https://github.com/GITHUB_USER/azure-ai-translator-accelerators"
+$repositoryUrl = "https://github.com/thomasdrewthomas/azure-ai-translator-accelerators"
 $branchName = "main"
 $webAppFolder = "document-translate-web"
 $accessToken = "" # Replace this with your GitHub access token
-$staticWebAppName = "ai-translator-static-webapp-$randomString"
+$staticWebAppName = "ai-translator-static-webapp-vwuif"
 $staticSitesRegion = "westeurope" # Adjust as needed
 
 # APIM variables
-$ApiManagementName = "translator-apim-$randomString"
-$PublisherEmail = "your-email@example.com"
-$PublisherName = "Your Company Name"
+$ApiManagementName = "translator-apim-vwuif"
+$PublisherEmail = "memiset@hotmail.com"
+$PublisherName = "msft"
