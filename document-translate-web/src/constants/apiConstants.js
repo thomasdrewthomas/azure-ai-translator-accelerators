55985a9854d5461f507c43bd26ccbb0d2aad07ed
thomasdrewthomas <117202574+thomasdrewthomas@users.noreply.github.com>
2024-09-23 18:00:44 +0100

    update-vars

--- document-translate-web/src/constants/apiConstants.js
+++ document-translate-web/src/constants/apiConstants.js
@@ -1,5 +1,5 @@
-export const API_KEY = process.env.REACT_APP_API_KEY;
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_KEY = "202ecdadb21c49f2b5edc50fb7cd6660";
+export const BASE_URL = "https://translator-apim-vwuif.azure-api.net/translation-service";
 
 export const GET_LOGS_API = (date) => `${BASE_URL}/get_logs_by_date?date=${date}`;
 export const GET_ALL_LOGS = `${BASE_URL}/get_all_logs`;
