793d0c35d945d978123ed45a5d610fff1814157d
thomasdrewthomas <117202574+thomasdrewthomas@users.noreply.github.com>
2024-09-30 14:27:12 +0100

    sdf

--- deployment-scripts/terraform/backend.tf
+++ deployment-scripts/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform"
-    storage_account_name = "terraform19831233"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_azuread_auth     = true
+ # backend "azurerm" {
+  #  resource_group_name  = "terraform"
+   # storage_account_name = "terraform19831233"
+    #container_name       = "tfstate"
+    #key                  = "terraform.tfstate"
+    #use_azuread_auth     = true
   }
 }
