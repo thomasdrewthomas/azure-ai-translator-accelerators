919ab888919265ef9a4f8064658160b8619d08cf
Moustafa Mahmoud <mustafaalaa.mohamed@gmail.com>
2024-10-10 12:53:35 +0100

    Fix unique variables bugs

--- deployment-scripts/terraform/cosmos_db.tf
+++ deployment-scripts/terraform/cosmos_db.tf
@@ -5,22 +5,22 @@ resource "random_password" "db_password" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 resource "azurerm_postgresql_flexible_server" "translator_db" {
-  name     = "${local.name_prefix}-transaltor-db-${var.random_string}"
-  location = azurerm_resource_group.rg.location
+  name                = "${local.name_prefix}-transaltor-db-${random_string.unique.result}"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   # Administrator details
   administrator_login    = var.postgres_administrator_login
-  administrator_password = random_password.db_password.result  # Use the generated password
+  administrator_password = random_password.db_password.result # Use the generated password
 
   # Serverless Configuration
-  sku_name = "B_Standard_B1ms" # Serverless SKU
-  storage_mb = 32768           # 32 GB minimum storage
-  version = "13"            # PostgreSQL version
+  sku_name   = "B_Standard_B1ms" # Serverless SKU
+  storage_mb = 32768             # 32 GB minimum storage
+  version    = "13"              # PostgreSQL version
 
   # Public Access Configuration
   public_network_access_enabled = true # Enable public access
-  zone = "2"
+  zone                          = "2"
 
   # Backup Retention
   backup_retention_days = 35
@@ -46,9 +46,9 @@ resource "azurerm_postgresql_flexible_server_database" "citus_db" {
 
 # # Firewall Rule to allow public IP access
 resource "azurerm_postgresql_flexible_server_firewall_rule" "example" {
-  name           = "allow-public-ip"
-  start_ip_address = "0.0.0.0"   # Allow all IPs (not recommended for production)
-  end_ip_address = "255.255.255.255"
-  server_id      = azurerm_postgresql_flexible_server.translator_db.id
+  name             = "allow-public-ip"
+  start_ip_address = "0.0.0.0" # Allow all IPs (not recommended for production)
+  end_ip_address   = "255.255.255.255"
+  server_id        = azurerm_postgresql_flexible_server.translator_db.id
 }
 
